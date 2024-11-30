08c81a8a3ffddb0d23dbd22d3dcb99ed3a470f82
Matthew James <matt@192-168-1-105.tpgi.com.au>
2024-11-30 10:54:20 +0800

    local migrate script

--- bin/local-migrate.sh
+++ bin/local-migrate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx stepwise-migrations migrate \
+npx --yes stepwise-migrations migrate \
   --connection=postgresql://postgres:postgres@127.0.0.1:5432/mydb \
   --schema=myschema \
   --path=./migrations/
